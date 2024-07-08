507303761d6c81e86e7f809fec06e087f4576567
ANTHO_XB <anthony_xb@proton.me>
2024-07-08 16:58:22 +0200

    Modif_exercices_PHP1

---
+++ exo8.php
@@ -0,0 +1,16 @@
+<h1>Exercice 8</h1>
+
+<p>
+Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la 
+forme :<br>
+<br>
+</p>
+
+<h2>
+Affichage (pour la table de 8) : <br>
+Table de 8 :
+1 x 8 = 8
+2 x 8 = 16
+3 x 8 = 24 …
+
+</h2>
