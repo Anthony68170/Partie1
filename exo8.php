ddddd930964df1f987e178713490628528ac727c
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-09 14:35:13 +0200

    Modif Exo_8_PHP

--- exo8.php
+++ exo8.php
@@ -8,17 +8,31 @@ forme :<br>
 
 <h2>
 Affichage (pour la table de 8) : <br>
-Table de 8 :
-1 x 8 = 8
-2 x 8 = 16
-3 x 8 = 24 …
+Table de 8 : <br>
+1 x 8 = 8 <br>
+2 x 8 = 16 <br>
+3 x 8 = 24 … <br>
 
 </h2>
 
 <?php
 
-$nbChoisi = "";
+/*$range = range(1,10);
+var_dump($range);
 
+foreach(range(1,8) as $valeur){
+    echo $valeur . " ";
+}*/
 
-
+echo "Table de 8 :<br>";
+echo "1 x 8 = 8 <br>";
+echo "2 x 8 = 16 <br>";
+echo "3 x 8 = 824 <br>";
+echo "4 x 8 = 32 <br>";
+echo "5 x 8 = 40 <br>";
+echo "6 x 8 = 48 <br>";
+echo "7 x 8 = 56 <br>";
+echo "8 x 8 = 64 <br>";
+echo "9 x 8 = 72 <br>";
+echo "10 x 8 = 80";
 ?>
