a704f95e8b2992618a676fd9d57c034772324275
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-10 21:04:14 +0200

    correction Exo13 PHP

--- Exo13.php
+++ Exo13.php
@@ -9,39 +9,49 @@
 
 <h1>Exercice 13</h1>
 
-<p>Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de coefficient).<br>
+<p>Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau 
+    (pas de coefficient).<br>
 Elle devra être affichée avec 2 décimales.</p>
 
 <h2> Résultat</h2>
 
 <?php
 
-$tabNotes = [10,12,8,19,3,16,11,13,9]; // PLACER LES NOTES DANS UN TABLEAU
-$nbNotes = count($tabNotes); // COMPTER LE NB DE NOTES DANS LE TABLEAU
+// PLACER LES NOTES DANS UN TABLEAU
+$tabNotes = [10,12,8,19,3,16,11,13,9]; 
+
+
+// AFFICHAGE DES NOTES DU TABLEAU 
+echo "Les notes obtenues par les élèves sont : ";
+foreach ($tabNotes as $note) {
+    echo $note . " ";
+}
+
+echo "<br>";
+
+// COMPTER LE NB DE NOTES DANS LE TABLEAU
+$nbNotes = count($tabNotes); 
 
 $total = 0;
-$moyenne = 0;
 
 foreach ($tabNotes as $valeur) {
-    $total = $total + $valeur;
+    $total += $valeur;
 }
-// echo $somme ;
 
-echo "Les notes obtenues par les élèves sont : <br>";
-round($total/count($tabNotes), 2);
+// CALCUL DE LA MOYENNE
+$moyenne = round($total / $nbNotes, 2);
 
+
+// AFFICHER LA DERNIERE REPONSE / RESULTAT 
 echo "Sa moyenne générale est donc de : $moyenne";
 
-// list ($tabNotes);
-// var_dump($tabNotes)
-// print_r($tabNotes)
-// $sommeNotes = array_sum($tabNotes);
-// $moyenneNotes = $sommeNotes / $nbNotes;
-// $arrondiMoyenne = round($moyenneNotes, 2);
 
 
 ?>
 
 </body>
 </html>
+
+
+
 
